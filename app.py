3ddab8cf38906abc213bc634e5f28fa07dcb9bc8
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2023-07-04 11:44:01 -0600

    first commit

--- app.py
+++ app.py
@@ -102,11 +102,11 @@ def load_license():
 
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     # app.run(threaded=True, port=5000)
-    #app.run(host="0.0.0.0", port=5000)
+   # app.run(host="0.0.0.0", port=5000)
     #app.run()
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=5000)
     
